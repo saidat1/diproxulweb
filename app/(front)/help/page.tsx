a97c76d4df5f27e9bdb081a600c272695bb25511
lenaz-wyt <lenazwyt222@gmail.com>
2025-01-23 16:49:33 +0200

    fixed incorrect helpPage import

--- app/(front)/help/page.tsx
+++ app/(front)/help/page.tsx
@@ -1,4 +1,4 @@
-import HelpPage from '@/components/frontend/help'
+import HelpPage from '@/components/frontend/(marketing)/resources/help/page';
 import React from 'react'
 
 export default function page() {
